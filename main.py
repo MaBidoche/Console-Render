94e34ccbe70963168ef21654e56b6def73bf06e2
Yannis <52321382+yannis300307@users.noreply.github.com>
2024-09-24 18:44:10 +0200

    Mega Optimisation

--- main.py
+++ main.py
@@ -15,18 +15,21 @@ while success:
 
     print("\033[1A"*rows, end="")
 
-    for y in range(rows):
-        for x in range(cols):
-            b, g, r = image[y, x]
-            print(f'\033[38;2;255;82;197;48;2;{r};{g};{b}m  ', end="", flush=False)
-
-            #Utiliser les charactères ▀ et ▄ pour préciser le dessin
+    for y in range(0, rows, 2):
+        for x in range(0, cols, 2):
+            b1, g1, r1 = image[y, x]
+            b2, g2, r2 = image[y+1, x]
+            print(f'\033[38;2;{r2};{g2};{b2};48;2;{r1};{g1};{b1}m▄', end="", flush=False)
+            b1, g1, r1 = image[y, x+1]
+            b2, g2, r2 = image[y + 1, x+1]
+            print(f'\033[38;2;{r2};{g2};{b2};48;2;{r1};{g1};{b1}m▄', end="", flush=False)
 
         print(RETURN, flush=False)
 
     count += 1
 
     sys.stdout.flush()
+
 
 vidcap.release()
 
